8062e1f91b0d8d694b4f2d3651c05d2515fb208c
RahulKrishna15 <rahulkrishnapooboni057@gmail.com>
2024-05-22 06:05:41 +0530

    first commit

---
+++ app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import { SiEthereum } from "react-icons/si";
+export default function Footer() {
+  return (
+    <footer className="bg-pink-500 p-4">
+      <div className="md:flex md:justify-between items-center">
+        
+
+       
+      </div>
+    </footer>
+  );
+}
---
+++ app/src/main.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Details from "./components/Details.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/details/:id",
+    element: <Details />,
+  },
+]);
+const activeChain = {
+  chainId: 1442,
+  rpc: ["https://polygon-zkevm-testnet.rpc.thirdweb.com"],
+
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  shortName: "zkEVM testnet",
+  slug: "zkevmtest",
+  testnet: true,
+  chain: "Polygon",
+  name: "Polygon zkEVM Testnet",
+};
+const clientId = process.env.REACT_APP_THIRDWEB_CLIENT_ID;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThirdwebProvider
+      activeChain={activeChain}
+      clientId={"7cb6a21a6fdb34756b1ba4482882e2ab"}
+      chainId={1442}
+      supportedChains={[activeChain]}
+      autoConnect={false}
+    >
+      <RouterProvider router={router} />
+    </ThirdwebProvider>
+  </React.StrictMode>
+);
